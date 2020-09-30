cae7097a661c4aa0edde92c9e7102318fe670ec4
Becca <becca@adadevelopersacademy.org>
2020-09-30 15:33:40 -0700

    driver

---
+++ lib/driver.rb
@@ -0,0 +1,27 @@
+require_relative 'csv_record'
+
+module RideShare
+  class Driver < CsvRecord
+    attr_reader :name, :vin, :status, :trips
+
+    def initialize(id:, name:, vin:, status:, trips: nil)
+      super(id)
+
+      @name = name
+      @vin = vin
+      @status = status
+      @trips = trips || []
+    end
+
+    private
+
+    def self.from_csv(record)
+      return new(
+          id: record[:id],
+          name: record[:name],
+          vin: record[:vin],
+          status: record[:status]
+      )
+    end
+  end
+end
